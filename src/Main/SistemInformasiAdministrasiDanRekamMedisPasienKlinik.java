03df355eaa54433bbd0fea9427103b9cc047f7c7
septianacoding <130918578+septianacoding@users.noreply.github.com>
2025-05-17 14:01:54 +0700

    Add files via upload

---
+++ src/Main/SistemInformasiAdministrasiDanRekamMedisPasienKlinik.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+import Gui.form_login;
+
+/**
+ *
+ * @author Deo
+ */
+public class SistemInformasiAdministrasiDanRekamMedisPasienKlinik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("program dimulai...");
+        form_login fl = new form_login();
+        fl.setVisible(true);
+        System.out.println("form login ditampilkan...");
+    }
+    
+}
